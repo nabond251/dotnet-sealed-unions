a92b6de0174ddd5b4c49ca69752daa51c1e4c3f6
Nathaniel Bond <nabond251@gmail.com>
2022-11-16 22:20:24 -0500

    Add tennis game types

---
+++ tests/SdgApps.Common.DotnetSealedUnions.Tests/TennisGame.cs
@@ -0,0 +1,269 @@
+﻿// <copyright file="TennisGame.cs" company="Soli Deo Gloria Apps">
+// Copyright (c) Soli Deo Gloria Apps. All rights reserved.
+// </copyright>
+
+namespace SdgApps.Common.DotnetSealedUnions.Tests;
+
+/// <summary>
+/// Tennis kata tennis game.
+/// </summary>
+public class TennisGame
+{
+    /// <summary>
+    /// Tennis score.
+    /// </summary>
+    public interface IScore
+    {
+        /// <summary>
+        /// Gets score.
+        /// </summary>
+        object Score { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IScore"/> interface
+        /// with given player points values.
+        /// </summary>
+        /// <param name="playerOnePoints">Player one points value.</param>
+        /// <param name="playerTwoPoints">Player two points value.</param>
+        /// <returns>
+        /// A new instance of the <see cref="IScore"/> interface with given
+        /// player points values.
+        /// </returns>
+        static IScore Points(IPlayerPoints playerOnePoints, IPlayerPoints playerTwoPoints)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IScore"/> interface
+        /// with advantage score.
+        /// </summary>
+        /// <param name="advantage">Advantage score.</param>
+        /// <returns>
+        /// A new instance of the <see cref="IScore"/> interface with advantage
+        /// score.
+        /// </returns>
+        static IScore Advantage(IAdvantage advantage)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IScore"/> interface
+        /// with deuce score.
+        /// </summary>
+        /// <returns>
+        /// A new instance of the <see cref="IScore"/> interface with deuce
+        /// score.
+        /// </returns>
+        static IScore Deuce()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IScore"/> interface
+        /// with game score.
+        /// </summary>
+        /// <param name="game">Game score.</param>
+        /// <returns>
+        /// A new instance of the <see cref="IScore"/> interface with game
+        /// score.
+        /// </returns>
+        static IScore Game(IGame game)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets string representation of <see cref="IScore"/>.
+        /// </summary>
+        /// <param name="score">
+        /// <see cref="IScore"/> whose string representation to get.
+        /// </param>
+        /// <returns>
+        /// String representation of <paramref name="score"/>.
+        /// </returns>
+        static string GetString(IScore score)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// Tennis player points.
+    /// </summary>
+    public interface IPlayerPoints
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IPlayerPoints"/>
+        /// interface with zero points.
+        /// </summary>
+        /// <returns>
+        /// A new instance of the <see cref="IPlayerPoints"/> interface with
+        /// zero points.
+        /// </returns>
+        static IPlayerPoints Zero()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IPlayerPoints"/>
+        /// interface with fifteen points.
+        /// </summary>
+        /// <returns>
+        /// A new instance of the <see cref="IPlayerPoints"/> interface with
+        /// fifteen points.
+        /// </returns>
+        static IPlayerPoints Fifteen()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IPlayerPoints"/>
+        /// interface with thirty points.
+        /// </summary>
+        /// <returns>
+        /// A new instance of the <see cref="IPlayerPoints"/> interface with
+        /// thirty points.
+        /// </returns>
+        static IPlayerPoints Thirty()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IPlayerPoints"/>
+        /// interface with forty points.
+        /// </summary>
+        /// <returns>
+        /// A new instance of the <see cref="IPlayerPoints"/> interface with
+        /// forty points.
+        /// </returns>
+        static IPlayerPoints Forty()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// Advantage score.
+    /// </summary>
+    public interface IAdvantage : IPlayer
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IAdvantage"/> interface
+        /// for player one.
+        /// </summary>
+        /// <returns>
+        /// A new instance of the <see cref="IAdvantage"/> interface for player
+        /// one.
+        /// </returns>
+        static new IAdvantage One()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IAdvantage"/> interface
+        /// for player two.
+        /// </summary>
+        /// <returns>
+        /// A new instance of the <see cref="IAdvantage"/> interface for player
+        /// two.
+        /// </returns>
+        static new IAdvantage Two()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// Game score.
+    /// </summary>
+    public interface IGame : IPlayer
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IGame"/> interface for
+        /// player one.
+        /// </summary>
+        /// <returns>
+        /// A new instance of the <see cref="IGame"/> interface for player one.
+        /// </returns>
+        static new IGame One()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IGame"/> interface for
+        /// player two.
+        /// </summary>
+        /// <returns>
+        /// A new instance of the <see cref="IGame"/> interface for player two.
+        /// </returns>
+        static new IGame Two()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// Tennis player.
+    /// </summary>
+    public interface IPlayer
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IPlayer"/> interface
+        /// for player one.
+        /// </summary>
+        /// <returns>
+        /// A new instance of the <see cref="IPlayer"/> interface for player
+        /// one.
+        /// </returns>
+        static IPlayer One()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IPlayer"/> interface
+        /// for player two.
+        /// </summary>
+        /// <returns>
+        /// A new instance of the <see cref="IPlayer"/> interface for player
+        /// two.
+        /// </returns>
+        static IPlayer Two()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets string representation of <see cref="IPlayer"/>.
+        /// </summary>
+        /// <param name="player">
+        /// <see cref="IPlayer"/> whose string representation to get.
+        /// </param>
+        /// <returns>
+        /// String representation of <paramref name="player"/>.
+        /// </returns>
+        static string GetString(IPlayer player)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// Scores a point for given player.
+    /// </summary>
+    /// <param name="score">Current score.</param>
+    /// <param name="player">Player for whom the point is scored.</param>
+    /// <returns>New score.</returns>
+    public static IScore ScorePoint(IScore score, IPlayer player)
+    {
+        throw new NotImplementedException();
+    }
+}
--- tests/SdgApps.Common.DotnetSealedUnions.Tests/TennisGameTest.cs
+++ tests/SdgApps.Common.DotnetSealedUnions.Tests/TennisGameTest.cs
@@ -2,50 +2,51 @@
 // Copyright (c) Soli Deo Gloria Apps. All rights reserved.
 // </copyright>
 
-namespace SdgApps.Common.DotnetSealedUnions.Tests
+namespace SdgApps.Common.DotnetSealedUnions.Tests;
+
+using static SdgApps.Common.DotnetSealedUnions.Tests.TennisGame;
+
+/// <summary>
+/// Tests sealed unions using tennis kata.
+/// </summary>
+public class TennisGameTest
 {
     /// <summary>
-    /// Tests sealed unions using tennis kata.
+    /// Runs a tennis kata.
     /// </summary>
-    public class TennisGameTest
+    [Fact]
+    public void TestTennis()
     {
-        /// <summary>
-        /// Runs a tennis kata.
-        /// </summary>
-        [Fact]
-        public void TestTennis()
-        {
-            Score start = Score.Points(PlayerPoints.Zero(), PlayerPoints.Zero());
-            Score fifteenZero = UpdateAndPrintScore(start, Player.One());
-            Score thirtyZero = UpdateAndPrintScore(fifteenZero, Player.One());
-            Score fortyZero = UpdateAndPrintScore(thirtyZero, Player.One());
-            Assert.Equal(
-                Score.Points(PlayerPoints.Forty(), PlayerPoints.Zero()).Score,
-                fortyZero.Score);
-            Score fortyThirty = UpdateAndPrintScore(ScorePoint(fortyZero, Player.Two()), Player.Two());
-            Assert.Equal(
-                Score.Points(PlayerPoints.Forty(), PlayerPoints.Thirty()).Score,
-                fortyThirty.Score);
-            Score deuce1 = UpdateAndPrintScore(fortyThirty, Player.Two());
-            Assert.Equal(Score.Deuce().Score, deuce1.Score);
-            Score advantageP1 = UpdateAndPrintScore(deuce1, Player.One());
-            Assert.Equal(Score.Advantage(Advantage.One()).Score, advantageP1.Score);
-            Score gameP1 = UpdateAndPrintScore(advantageP1, Player.One());
-            Assert.Equal(Score.Game(Game.One()).Score, gameP1.Score);
-            Score zeroFifteen = UpdateAndPrintScore(gameP1, Player.Two());
-            Assert.Equal(
-                Score.Points(PlayerPoints.Zero(), PlayerPoints.Fifteen()).Score,
-                zeroFifteen.Score);
-            Score deuce2 = UpdateAndPrintScore(advantageP1, Player.Two());
-            Assert.Equal(Score.Deuce().Score, deuce2.Score);
-        }
+        IScore start = IScore.Points(IPlayerPoints.Zero(), IPlayerPoints.Zero());
+        IScore fifteenZero = UpdateAndPrintScore(start, IPlayer.One());
+        IScore thirtyZero = UpdateAndPrintScore(fifteenZero, IPlayer.One());
+        IScore fortyZero = UpdateAndPrintScore(thirtyZero, IPlayer.One());
+        Assert.Equal(
+            IScore.Points(IPlayerPoints.Forty(), IPlayerPoints.Zero()).Score,
+            fortyZero.Score);
+        IScore fortyThirty = UpdateAndPrintScore(ScorePoint(fortyZero, IPlayer.Two()), IPlayer.Two());
+        Assert.Equal(
+            IScore.Points(IPlayerPoints.Forty(), IPlayerPoints.Thirty()).Score,
+            fortyThirty.Score);
+        IScore deuce1 = UpdateAndPrintScore(fortyThirty, IPlayer.Two());
+        Assert.Equal(IScore.Deuce().Score, deuce1.Score);
+        IScore advantageP1 = UpdateAndPrintScore(deuce1, IPlayer.One());
+        Assert.Equal(IScore.Advantage(IAdvantage.One()).Score, advantageP1.Score);
+        IScore gameP1 = UpdateAndPrintScore(advantageP1, IPlayer.One());
+        Assert.Equal(IScore.Game(IGame.One()).Score, gameP1.Score);
+        IScore zeroFifteen = UpdateAndPrintScore(gameP1, IPlayer.Two());
+        Assert.Equal(
+            IScore.Points(IPlayerPoints.Zero(), IPlayerPoints.Fifteen()).Score,
+            zeroFifteen.Score);
+        IScore deuce2 = UpdateAndPrintScore(advantageP1, IPlayer.Two());
+        Assert.Equal(IScore.Deuce().Score, deuce2.Score);
+    }
 
-        private Score UpdateAndPrintScore(Score oldScore, Player player)
-        {
-            Score newScore = ScorePoint(oldScore, player);
-            Console.WriteLine(Score.GetString(oldScore) + " + " + Player.GetString(player) + " -> "
-                + Score.GetString(newScore));
-            return newScore;
-        }
+    private static IScore UpdateAndPrintScore(IScore oldScore, IPlayer player)
+    {
+        IScore newScore = ScorePoint(oldScore, player);
+        Console.WriteLine(IScore.GetString(oldScore) + " + " + IPlayer.GetString(player) + " -> "
+            + IScore.GetString(newScore));
+        return newScore;
     }
 }
