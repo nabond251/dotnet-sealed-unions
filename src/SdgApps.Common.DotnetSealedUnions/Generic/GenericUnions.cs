0183b7527bbdef5b1a9d5fcc8c7fa594fe5fb8e4
Nathaniel Bond <nabond251@gmail.com>
2022-12-02 18:00:14 -0500

    Add nonet factory

--- src/SdgApps.Common.DotnetSealedUnions/Generic/GenericUnions.cs
+++ src/SdgApps.Common.DotnetSealedUnions/Generic/GenericUnions.cs
@@ -41,6 +41,31 @@ public static class GenericUnions
         return new Union4Factory<TA, TB, TC, TD>();
     }
 
+    /// <summary>
+    /// Initializes a new instance of the
+    /// <see cref="IUnion9{TFirst, TSecond, TThird, TFourth, TFifth, TSixth, TSeventh, TEighth, TNinth}"/>
+    /// class.
+    /// </summary>
+    /// <typeparam name="TA">First possible type.</typeparam>
+    /// <typeparam name="TB">Second possible type.</typeparam>
+    /// <typeparam name="TC">Third possible type.</typeparam>
+    /// <typeparam name="TD">Fourth possible type.</typeparam>
+    /// <typeparam name="TE">Fifth possible type.</typeparam>
+    /// <typeparam name="TF">Sixth possible type.</typeparam>
+    /// <typeparam name="TG">Seventh possible type.</typeparam>
+    /// <typeparam name="TH">Eighth possible type.</typeparam>
+    /// <typeparam name="TI">Ninth possible type.</typeparam>
+    /// <returns>
+    /// A new instance of the
+    /// <see cref="IUnion9{TFirst, TSecond, TThird, TFourth, TFifth, TSixth, TSeventh, TEighth, TNinth}"/>
+    /// class.
+    /// </returns>
+    public static IUnion9<TA, TB, TC, TD, TE, TF, TG, TH, TI>.IFactory
+        NonetFactory<TA, TB, TC, TD, TE, TF, TG, TH, TI>()
+    {
+        return new Union9Factory<TA, TB, TC, TD, TE, TF, TG, TH, TI>();
+    }
+
     private sealed class Union2Factory<TA, TB> : IUnion2<TA, TB>.IFactory
     {
         public IUnion2<TA, TB> First(TA value)
@@ -74,6 +99,55 @@ public static class GenericUnions
         public IUnion4<TA, TB, TC, TD> Fourth(TD value)
         {
             return new Union4Fourth<TA, TB, TC, TD>(value);
+        }
+    }
+
+    private sealed class Union9Factory<TA, TB, TC, TD, TE, TF, TG, TH, TI>
+        : IUnion9<TA, TB, TC, TD, TE, TF, TG, TH, TI>.IFactory
+    {
+        public IUnion9<TA, TB, TC, TD, TE, TF, TG, TH, TI> First(TA value)
+        {
+            return new Union9First<TA, TB, TC, TD, TE, TF, TG, TH, TI>(value);
+        }
+
+        public IUnion9<TA, TB, TC, TD, TE, TF, TG, TH, TI> Second(TB value)
+        {
+            return new Union9Second<TA, TB, TC, TD, TE, TF, TG, TH, TI>(value);
+        }
+
+        public IUnion9<TA, TB, TC, TD, TE, TF, TG, TH, TI> Third(TC value)
+        {
+            return new Union9Third<TA, TB, TC, TD, TE, TF, TG, TH, TI>(value);
+        }
+
+        public IUnion9<TA, TB, TC, TD, TE, TF, TG, TH, TI> Fourth(TD value)
+        {
+            return new Union9Fourth<TA, TB, TC, TD, TE, TF, TG, TH, TI>(value);
+        }
+
+        public IUnion9<TA, TB, TC, TD, TE, TF, TG, TH, TI> Fifth(TE value)
+        {
+            return new Union9Fifth<TA, TB, TC, TD, TE, TF, TG, TH, TI>(value);
+        }
+
+        public IUnion9<TA, TB, TC, TD, TE, TF, TG, TH, TI> Sixth(TF value)
+        {
+            return new Union9Sixth<TA, TB, TC, TD, TE, TF, TG, TH, TI>(value);
+        }
+
+        public IUnion9<TA, TB, TC, TD, TE, TF, TG, TH, TI> Seventh(TG value)
+        {
+            return new Union9Seventh<TA, TB, TC, TD, TE, TF, TG, TH, TI>(value);
+        }
+
+        public IUnion9<TA, TB, TC, TD, TE, TF, TG, TH, TI> Eighth(TH value)
+        {
+            return new Union9Eighth<TA, TB, TC, TD, TE, TF, TG, TH, TI>(value);
+        }
+
+        public IUnion9<TA, TB, TC, TD, TE, TF, TG, TH, TI> Ninth(TI value)
+        {
+            return new Union9Ninth<TA, TB, TC, TD, TE, TF, TG, TH, TI>(value);
         }
     }
 }
--- tests/SdgApps.Common.DotnetSealedUnions.Tests/Generic/GenericUnionsTest.cs
+++ tests/SdgApps.Common.DotnetSealedUnions.Tests/Generic/GenericUnionsTest.cs
@@ -52,48 +52,184 @@ public class GenericUnionsTest
     [Fact]
     public void TestJoin()
     {
-        IUnion2<int, int>.IFactory doubletFactory = GenericUnions.DoubletFactory<int, int>();
-        IUnion4<int, int, int, int>.IFactory quartetFactory = GenericUnions
-            .QuartetFactory<int, int, int, int>();
-        string join2 = doubletFactory.First(0).Join(VALUE, EMPTY);
-        string join4 = quartetFactory.First(0).Join(VALUE, EMPTY, EMPTY, EMPTY);
-        Assert.Equal(
-            ValidArray,
-            new List<string> { join2, join4 });
-        join2 = doubletFactory.Second(0).Join(EMPTY, VALUE);
-        join4 = quartetFactory.Second(0).Join(EMPTY, VALUE, EMPTY, EMPTY);
-        Assert.Equal(
-            ValidArray,
-            new List<string> { join2, join4 });
-        join4 = quartetFactory.Third(0).Join(EMPTY, EMPTY, VALUE, EMPTY);
-        Assert.Equal(
-            ValidArray,
-            new List<string> { join2, join4 });
-        join4 = quartetFactory.Fourth(0).Join(EMPTY, EMPTY, EMPTY, VALUE);
-        Assert.Equal(
-            ValidArray,
-            new List<string> { join2, join4 });
+        IUnion0<int>.IFactory nulletFactory = GenericUnions.nulletFactory<int>();
+        IUnion1.IFactory<int> singletFactory = GenericUnions.singletFactory();
+        IUnion2.IFactory<int, int> doubletFactory = GenericUnions.doubletFactory();
+        IUnion3.IFactory<int, int, int> tripletFactory = GenericUnions.tripletFactory();
+        IUnion4.IFactory<int, int, int, int> quartetFactory = GenericUnions
+                .quartetFactory();
+        IUnion5.IFactory<int, int, int, int, int> quintetFactory = GenericUnions
+                .quintetFactory();
+        IUnion6.IFactory<int, int, int, int, int, int> sextetFactory = GenericUnions
+                .sextetFactory();
+        IUnion7.IFactory<int, int, int, int, int, int, int> septetFactory = GenericUnions
+                .septetFactory();
+        IUnion8.IFactory<int, int, int, int, int, int, int, int> octetFactory = GenericUnions
+                .octetFactory();
+        IUnion9.IFactory<int, int, int, int, int, int, int, int, int> nonetFactory = GenericUnions
+                .nonetFactory();
+        String join0 = nulletFactory.first(0).join(VALUE);
+        String join1 = singletFactory.first(0).join(VALUE, () -> INVALID);
+        String join2 = doubletFactory.first(0).join(VALUE, EMPTY);
+        String join3 = tripletFactory.first(0).join(VALUE, EMPTY, EMPTY);
+        String join4 = quartetFactory.first(0).join(VALUE, EMPTY, EMPTY, EMPTY);
+        String join5 = quintetFactory.first(0).join(VALUE, EMPTY, EMPTY, EMPTY, EMPTY);
+        String join6 = sextetFactory.first(0).join(VALUE, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY);
+        String join7 = septetFactory.first(0).join(VALUE, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY);
+        String join8 = octetFactory.first(0).join(VALUE, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,
+                EMPTY);
+        String join9 = nonetFactory.first(0).join(VALUE, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,
+                EMPTY, EMPTY);
+        Assert.assertEquals(VALID_ARRAY,
+                Arrays.asList(join0, join1, join2, join3, join4, join5, join6, join7, join8, join9));
+        join1 = singletFactory.none().join(EMPTY, () -> VALID);
+        join2 = doubletFactory.second(0).join(EMPTY, VALUE);
+        join3 = tripletFactory.second(0).join(EMPTY, VALUE, EMPTY);
+        join4 = quartetFactory.second(0).join(EMPTY, VALUE, EMPTY, EMPTY);
+        join5 = quintetFactory.second(0).join(EMPTY, VALUE, EMPTY, EMPTY, EMPTY);
+        join6 = sextetFactory.second(0).join(EMPTY, VALUE, EMPTY, EMPTY, EMPTY, EMPTY);
+        join7 = septetFactory.second(0).join(EMPTY, VALUE, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY);
+        join8 = octetFactory.second(0).join(EMPTY, VALUE, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY);
+        join9 = nonetFactory.second(0).join(EMPTY, VALUE, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,
+                EMPTY);
+        Assert.assertEquals(VALID_ARRAY,
+                Arrays.asList(join0, join1, join2, join3, join4, join5, join6, join7, join8, join9));
+        join3 = tripletFactory.third(0).join(EMPTY, EMPTY, VALUE);
+        join4 = quartetFactory.third(0).join(EMPTY, EMPTY, VALUE, EMPTY);
+        join5 = quintetFactory.third(0).join(EMPTY, EMPTY, VALUE, EMPTY, EMPTY);
+        join6 = sextetFactory.third(0).join(EMPTY, EMPTY, VALUE, EMPTY, EMPTY, EMPTY);
+        join7 = septetFactory.third(0).join(EMPTY, EMPTY, VALUE, EMPTY, EMPTY, EMPTY, EMPTY);
+        join8 = octetFactory.third(0).join(EMPTY, EMPTY, VALUE, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY);
+        join9 = nonetFactory.third(0).join(EMPTY, EMPTY, VALUE, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,
+                EMPTY);
+        Assert.assertEquals(VALID_ARRAY,
+                Arrays.asList(join0, join1, join2, join3, join4, join5, join6, join7, join8, join9));
+        join4 = quartetFactory.fourth(0).join(EMPTY, EMPTY, EMPTY, VALUE);
+        join5 = quintetFactory.fourth(0).join(EMPTY, EMPTY, EMPTY, VALUE, EMPTY);
+        join6 = sextetFactory.fourth(0).join(EMPTY, EMPTY, EMPTY, VALUE, EMPTY, EMPTY);
+        join7 = septetFactory.fourth(0).join(EMPTY, EMPTY, EMPTY, VALUE, EMPTY, EMPTY, EMPTY);
+        join8 = octetFactory.fourth(0).join(EMPTY, EMPTY, EMPTY, VALUE, EMPTY, EMPTY, EMPTY, EMPTY);
+        join9 = nonetFactory.fourth(0).join(EMPTY, EMPTY, EMPTY, VALUE, EMPTY, EMPTY, EMPTY, EMPTY,
+                EMPTY);
+        Assert.assertEquals(VALID_ARRAY,
+                Arrays.asList(join0, join1, join2, join3, join4, join5, join6, join7, join8, join9));
+        join5 = quintetFactory.fifth(0).join(EMPTY, EMPTY, EMPTY, EMPTY, VALUE);
+        join6 = sextetFactory.fifth(0).join(EMPTY, EMPTY, EMPTY, EMPTY, VALUE, EMPTY);
+        join7 = septetFactory.fifth(0).join(EMPTY, EMPTY, EMPTY, EMPTY, VALUE, EMPTY, EMPTY);
+        join8 = octetFactory.fifth(0).join(EMPTY, EMPTY, EMPTY, EMPTY, VALUE, EMPTY, EMPTY, EMPTY);
+        join9 = nonetFactory.fifth(0).join(EMPTY, EMPTY, EMPTY, EMPTY, VALUE, EMPTY, EMPTY, EMPTY,
+                EMPTY);
+        Assert.assertEquals(VALID_ARRAY,
+                Arrays.asList(join0, join1, join2, join3, join4, join5, join6, join7, join8, join9));
+        join6 = sextetFactory.sixth(0).join(EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, VALUE);
+        join7 = septetFactory.sixth(0).join(EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, VALUE, EMPTY);
+        join8 = octetFactory.sixth(0).join(EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, VALUE, EMPTY, EMPTY);
+        join9 = nonetFactory.sixth(0).join(EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, VALUE, EMPTY, EMPTY,
+                EMPTY);
+        Assert.assertEquals(VALID_ARRAY,
+                Arrays.asList(join0, join1, join2, join3, join4, join5, join6, join7, join8, join9));
+        join7 = septetFactory.seventh(0).join(EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, VALUE);
+        join8 = octetFactory.seventh(0).join(EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, VALUE,
+                EMPTY);
+        join9 = nonetFactory.seventh(0).join(EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, VALUE, EMPTY,
+                EMPTY);
+        Assert.assertEquals(VALID_ARRAY,
+                Arrays.asList(join0, join1, join2, join3, join4, join5, join6, join7, join8, join9));
+        join8 = octetFactory.eighth(0).join(EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, VALUE);
+        join9 = nonetFactory.eighth(0).join(EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, VALUE,
+                EMPTY);
+        Assert.assertEquals(VALID_ARRAY,
+                Arrays.asList(join0, join1, join2, join3, join4, join5, join6, join7, join8, join9));
+        join9 = nonetFactory.ninth(0).join(EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,
+                VALUE);
+        Assert.assertEquals(VALID_ARRAY,
+                Arrays.asList(join0, join1, join2, join3, join4, join5, join6, join7, join8, join9));
     }
 
-    /// <summary>
-    /// Tests Continue overloads.
-    /// </summary>
-    [Fact]
-    public void TestContinued()
-    {
-        IUnion2<int, int>.IFactory doubletFactory = GenericUnions.DoubletFactory<int, int>();
-        IUnion4<int, int, int, int>.IFactory quartetFactory = GenericUnions
-            .QuartetFactory<int, int, int, int>();
-        doubletFactory.First(0).Continued(SUCCESS, ERROR);
-        quartetFactory.First(0).Continued(SUCCESS, ERROR, ERROR, ERROR);
-
-        doubletFactory.Second(0).Continued(ERROR, SUCCESS);
-        quartetFactory.Second(0).Continued(ERROR, SUCCESS, ERROR, ERROR);
-
-        quartetFactory.Third(0).Continued(ERROR, ERROR, SUCCESS, ERROR);
-
-        quartetFactory.Fourth(0).Continued(ERROR, ERROR, ERROR, SUCCESS);
-
-        Assert.True(true);
+    @Test
+    public void testContinued() throws Exception {
+        IUnion0.IFactory<int> nulletFactory = GenericUnions.nulletFactory();
+        IUnion1.IFactory<int> singletFactory = GenericUnions.singletFactory();
+        IUnion2.IFactory<int, int> doubletFactory = GenericUnions.doubletFactory();
+        IUnion3.IFactory<int, int, int> tripletFactory = GenericUnions.tripletFactory();
+        IUnion4.IFactory<int, int, int, int> quartetFactory = GenericUnions
+                .quartetFactory();
+        IUnion5.IFactory<int, int, int, int, int> quintetFactory = GenericUnions
+                .quintetFactory();
+        IUnion6.IFactory<int, int, int, int, int, int> sextetFactory = GenericUnions
+                .sextetFactory();
+        IUnion7.IFactory<int, int, int, int, int, int, int> septetFactory = GenericUnions
+                .septetFactory();
+        IUnion8.IFactory<int, int, int, int, int, int, int, int> octetFactory = GenericUnions
+                .octetFactory();
+        IUnion9.IFactory<int, int, int, int, int, int, int, int, int> nonetFactory = GenericUnions
+                .nonetFactory();
+        nulletFactory.first(0).continued(SUCCESS);
+        singletFactory.first(0).continued(SUCCESS, () -> {
+            throw new IllegalStateException();
+        });
+        doubletFactory.first(0).continued(SUCCESS, ERROR);
+        tripletFactory.first(0).continued(SUCCESS, ERROR, ERROR);
+        quartetFactory.first(0).continued(SUCCESS, ERROR, ERROR, ERROR);
+        quintetFactory.first(0).continued(SUCCESS, ERROR, ERROR, ERROR, ERROR);
+        sextetFactory.first(0).continued(SUCCESS, ERROR, ERROR, ERROR, ERROR, ERROR);
+        septetFactory.first(0).continued(SUCCESS, ERROR, ERROR, ERROR, ERROR, ERROR, ERROR);
+        octetFactory.first(0).continued(SUCCESS, ERROR, ERROR, ERROR, ERROR, ERROR, ERROR, ERROR);
+        nonetFactory.first(0).continued(SUCCESS, ERROR, ERROR, ERROR, ERROR, ERROR, ERROR, ERROR,
+                ERROR);
+        /* */
+        singletFactory.none().continued(ERROR, () -> {
+        });
+        doubletFactory.second(0).continued(ERROR, SUCCESS);
+        tripletFactory.second(0).continued(ERROR, SUCCESS, ERROR);
+        quartetFactory.second(0).continued(ERROR, SUCCESS, ERROR, ERROR);
+        quintetFactory.second(0).continued(ERROR, SUCCESS, ERROR, ERROR, ERROR);
+        sextetFactory.second(0).continued(ERROR, SUCCESS, ERROR, ERROR, ERROR, ERROR);
+        septetFactory.second(0).continued(ERROR, SUCCESS, ERROR, ERROR, ERROR, ERROR, ERROR);
+        octetFactory.second(0).continued(ERROR, SUCCESS, ERROR, ERROR, ERROR, ERROR, ERROR, ERROR);
+        nonetFactory.second(0).continued(ERROR, SUCCESS, ERROR, ERROR, ERROR, ERROR, ERROR, ERROR,
+                ERROR);
+        /* */
+        tripletFactory.third(0).continued(ERROR, ERROR, SUCCESS);
+        quartetFactory.third(0).continued(ERROR, ERROR, SUCCESS, ERROR);
+        quintetFactory.third(0).continued(ERROR, ERROR, SUCCESS, ERROR, ERROR);
+        sextetFactory.third(0).continued(ERROR, ERROR, SUCCESS, ERROR, ERROR, ERROR);
+        septetFactory.third(0).continued(ERROR, ERROR, SUCCESS, ERROR, ERROR, ERROR, ERROR);
+        octetFactory.third(0).continued(ERROR, ERROR, SUCCESS, ERROR, ERROR, ERROR, ERROR, ERROR);
+        nonetFactory.third(0).continued(ERROR, ERROR, SUCCESS, ERROR, ERROR, ERROR, ERROR, ERROR,
+                ERROR);
+        /* */
+        quartetFactory.fourth(0).continued(ERROR, ERROR, ERROR, SUCCESS);
+        quintetFactory.fourth(0).continued(ERROR, ERROR, ERROR, SUCCESS, ERROR);
+        sextetFactory.fourth(0).continued(ERROR, ERROR, ERROR, SUCCESS, ERROR, ERROR);
+        septetFactory.fourth(0).continued(ERROR, ERROR, ERROR, SUCCESS, ERROR, ERROR, ERROR);
+        octetFactory.fourth(0).continued(ERROR, ERROR, ERROR, SUCCESS, ERROR, ERROR, ERROR, ERROR);
+        nonetFactory.fourth(0).continued(ERROR, ERROR, ERROR, SUCCESS, ERROR, ERROR, ERROR, ERROR,
+                ERROR);
+        /* */
+        quintetFactory.fifth(0).continued(ERROR, ERROR, ERROR, ERROR, SUCCESS);
+        sextetFactory.fifth(0).continued(ERROR, ERROR, ERROR, ERROR, SUCCESS, ERROR);
+        septetFactory.fifth(0).continued(ERROR, ERROR, ERROR, ERROR, SUCCESS, ERROR, ERROR);
+        octetFactory.fifth(0).continued(ERROR, ERROR, ERROR, ERROR, SUCCESS, ERROR, ERROR, ERROR);
+        nonetFactory.fifth(0).continued(ERROR, ERROR, ERROR, ERROR, SUCCESS, ERROR, ERROR, ERROR,
+                ERROR);
+        /* */
+        sextetFactory.sixth(0).continued(ERROR, ERROR, ERROR, ERROR, ERROR, SUCCESS);
+        septetFactory.sixth(0).continued(ERROR, ERROR, ERROR, ERROR, ERROR, SUCCESS, ERROR);
+        octetFactory.sixth(0).continued(ERROR, ERROR, ERROR, ERROR, ERROR, SUCCESS, ERROR, ERROR);
+        nonetFactory.sixth(0).continued(ERROR, ERROR, ERROR, ERROR, ERROR, SUCCESS, ERROR, ERROR,
+                ERROR);
+        /* */
+        septetFactory.seventh(0).continued(ERROR, ERROR, ERROR, ERROR, ERROR, ERROR, SUCCESS);
+        octetFactory.seventh(0).continued(ERROR, ERROR, ERROR, ERROR, ERROR, ERROR, SUCCESS, ERROR);
+        nonetFactory.seventh(0).continued(ERROR, ERROR, ERROR, ERROR, ERROR, ERROR, SUCCESS, ERROR,
+                ERROR);
+        /* */
+        octetFactory.eighth(0).continued(ERROR, ERROR, ERROR, ERROR, ERROR, ERROR, ERROR, SUCCESS);
+        nonetFactory.eighth(0).continued(ERROR, ERROR, ERROR, ERROR, ERROR, ERROR, ERROR, SUCCESS,
+                ERROR);
+        /* */
+        nonetFactory.ninth(0).continued(ERROR, ERROR, ERROR, ERROR, ERROR, ERROR, ERROR, ERROR,
+                SUCCESS);
     }
 }
