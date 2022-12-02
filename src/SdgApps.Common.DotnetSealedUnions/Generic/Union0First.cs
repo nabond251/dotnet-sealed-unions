f3bef7f1eb4d712ac1ae93f3cbcda3a202787372
Nathaniel Bond <nabond251@gmail.com>
2022-12-02 13:49:25 -0500

    Add Union0

---
+++ src/SdgApps.Common.DotnetSealedUnions/Generic/Union0First.cs
@@ -0,0 +1,59 @@
+﻿// <copyright file="Union0First.cs" company="Soli Deo Gloria Apps">
+// Copyright (c) Soli Deo Gloria Apps. All rights reserved.
+// </copyright>
+
+namespace SdgApps.Common.DotnetSealedUnions.Generic;
+
+/// <summary>
+/// <see cref="IUnion0{TFirst}"/>
+/// element with first of 1 possible type.
+/// </summary>
+/// <typeparam name="TA">First possible type.</typeparam>
+internal sealed class Union0First<TA>
+    : IUnion0<TA>
+{
+    private readonly TA value;
+
+    /// <summary>
+    /// Initializes a new instance of the
+    /// <see cref="Union0First{TA}"/> class.
+    /// </summary>
+    /// <param name="value">Element value.</param>
+    public Union0First(TA value)
+    {
+        this.value = value;
+    }
+
+    /// <inheritdoc/>
+    public void Continued(
+        Action<TA> continuationFirst)
+    {
+        continuationFirst(this.value);
+    }
+
+    /// <inheritdoc/>
+    public TR Join<TR>(
+        Func<TA, TR> mapFirst)
+    {
+        return mapFirst(this.value);
+    }
+
+    /// <inheritdoc/>
+    public override bool Equals(object? obj)
+    {
+        return obj is Union0First<TA> first &&
+            EqualityComparer<TA>.Default.Equals(this.value, first.value);
+    }
+
+    /// <inheritdoc/>
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(this.value);
+    }
+
+    /// <inheritdoc/>
+    public override string ToString()
+    {
+        return this.value?.ToString() ?? string.Empty;
+    }
+}
---
+++ src/SdgApps.Common.DotnetSealedUnions/IUnion0.cs
@@ -0,0 +1,57 @@
+﻿// <copyright file="IUnion0.cs" company="Soli Deo Gloria Apps">
+// Copyright (c) Soli Deo Gloria Apps. All rights reserved.
+// </copyright>
+
+namespace SdgApps.Common.DotnetSealedUnions;
+
+/// <summary>
+/// <see cref="IUnion0{TFirst}"/>
+/// represents a union containing an element of 1 possible type.
+/// </summary>
+/// <typeparam name="TFirst">First possible type.</typeparam>
+public interface IUnion0<
+    TFirst>
+{
+    /// <summary>
+    /// Creator class for
+    /// <see cref="IUnion0{TFirst}"/>.
+    /// </summary>
+    public interface IFactory
+    {
+        /// <summary>
+        /// Creates a Union0 wrapping a value of the first
+        /// <paramref name="value"/>.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>
+        /// A
+        /// <see cref="IUnion0{TFirst}"/>
+        /// object wrapping the value.
+        /// </returns>
+        IUnion0<
+            TFirst>
+            First(TFirst value);
+    }
+
+    /// <summary>
+    /// Executes one of the continuations depending on the element type.
+    /// </summary>
+    /// <param name="continuationFirst">
+    /// Continuation to execute if first possible type.
+    /// </param>
+    void Continued(
+        Action<TFirst> continuationFirst);
+
+    /// <summary>
+    /// Transforms the element in the union to a new type.
+    /// </summary>
+    /// <typeparam name="TR">Result type.</typeparam>
+    /// <param name="mapFirst">
+    /// Transformation to evaluate if first possible type.
+    /// </param>
+    /// <returns>
+    /// An object of the result type.
+    /// </returns>
+    TR Join<TR>(
+        Func<TFirst, TR> mapFirst);
+}
