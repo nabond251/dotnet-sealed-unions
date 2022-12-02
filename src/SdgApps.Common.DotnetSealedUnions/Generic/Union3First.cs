d7b335191bdb91571dcdff29efd3e569eab3775f
Nathaniel Bond <nabond251@gmail.com>
2022-12-02 13:38:50 -0500

    Add Union3

---
+++ src/SdgApps.Common.DotnetSealedUnions/Generic/Union3First.cs
@@ -0,0 +1,65 @@
+﻿// <copyright file="Union3First.cs" company="Soli Deo Gloria Apps">
+// Copyright (c) Soli Deo Gloria Apps. All rights reserved.
+// </copyright>
+
+namespace SdgApps.Common.DotnetSealedUnions.Generic;
+
+/// <summary>
+/// <see cref="IUnion3{TFirst, TSecond, TThird}"/>
+/// element with first of 3 possible types.
+/// </summary>
+/// <typeparam name="TA">First possible type.</typeparam>
+/// <typeparam name="TB">Second possible type.</typeparam>
+/// <typeparam name="TC">Third possible type.</typeparam>
+internal sealed class Union3First<TA, TB, TC>
+    : IUnion3<TA, TB, TC>
+{
+    private readonly TA value;
+
+    /// <summary>
+    /// Initializes a new instance of the
+    /// <see cref="Union3First{TA, TB, TC}"/> class.
+    /// </summary>
+    /// <param name="value">Element value.</param>
+    public Union3First(TA value)
+    {
+        this.value = value;
+    }
+
+    /// <inheritdoc/>
+    public void Continued(
+        Action<TA> continuationFirst,
+        Action<TB> continuationSecond,
+        Action<TC> continuationThird)
+    {
+        continuationFirst(this.value);
+    }
+
+    /// <inheritdoc/>
+    public TR Join<TR>(
+        Func<TA, TR> mapFirst,
+        Func<TB, TR> mapSecond,
+        Func<TC, TR> mapThird)
+    {
+        return mapFirst(this.value);
+    }
+
+    /// <inheritdoc/>
+    public override bool Equals(object? obj)
+    {
+        return obj is Union3First<TA, TB, TC> first &&
+            EqualityComparer<TA>.Default.Equals(this.value, first.value);
+    }
+
+    /// <inheritdoc/>
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(this.value);
+    }
+
+    /// <inheritdoc/>
+    public override string ToString()
+    {
+        return this.value?.ToString() ?? string.Empty;
+    }
+}
